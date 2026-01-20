6f07551855eb72b0fc465dcbb8d35ead726552d8
IsmailAsif4265 <ismailhossainasif4265@gmail.com>
2026-01-20 13:58:00 +0600

    Update FirstTest.py

--- FirstTest.py
+++ FirstTest.py
@@ -1,3 +1,21 @@
 import pygame
 
 x=pygame.init()
+
+#Creating Window
+gameWindow=pygame.display.set_mode((1200, 500))
+pygame.display.set_caption("My First Game")
+
+#Necessary Variables
+exit_game=False
+game_over=False
+
+#Game Loop
+while not exit_game:
+    for event in pygame.event.get():
+        print(event)
+        if event.type == pygame.QUIT:
+            exit_game=True
+
+pygame.quit()
+quit()
